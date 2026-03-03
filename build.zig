360b540db8f6526d625793206b3b12e1d6d19a94
Bin_Laptop <ethan.ramdeen64@gmail.com>
2026-03-03 18:26:46 -0500

    updated build.zig

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{.preferred_optimize_mode = .ReleaseFast});
+    const optimize = b.standardOptimizeOption(.{});
     const mod = b.addModule("blast", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
